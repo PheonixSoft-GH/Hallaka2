cd5620a48dbbff1913f09e1780854ea57112b6b6
Awais Ahmad <ranaawaisahmad.101@gmail.com>
2020-11-03 15:26:50 +0500

    comite

--- app/src/main/java/com/pheonix/org/hallaka/BarberActivity.java
+++ app/src/main/java/com/pheonix/org/hallaka/BarberActivity.java
@@ -1,16 +1,38 @@
 package com.pheonix.org.hallaka;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class BarberActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber);
 
+        imageView = findViewById(R.id.menuBtnBarber);
+        drawerLayout = findViewById(R.id.drawerLayBarber);
+        navigationView = findViewById(R.id.navLayBarber);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                drawerLayout.openDrawer(navigationView);
+
+            }
+        });
 
     }
 }
